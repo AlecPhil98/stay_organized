b564da83de796f69fb2458e0d98c613c8fc77c5a
Alec Philavong <alecphil@live.com>
2024-06-17 14:00:13 -0500

    add html inputs and add form data fucntion incomplete

--- scripts/new_todo.js
+++ scripts/new_todo.js
@@ -6,8 +6,6 @@ window.onload = () => {
 
     pickCategoryDropdown()
 
-    taskPriorityDropdown()
-
     const addTodoForm = document.querySelector("#addTodoForm");
 
     addTodoForm.addEventListener("submit", addTodo)
@@ -70,9 +68,37 @@ let pickCategoryDropdown = async () => {
 
 }
 
-let taskPriorityDropdown = () => {
+// Create todo 
+const addTodo = async (event) => {
+    // call preventDefault to keep the page from reloading the form and refreshing 
+    event.preventDefault();
+    console.log("alec")
+    //generate new form data object
+    let formData = new FormData(event.target);
+
+    // generate  a javascript Objext from the form data object created above 
+    let formDataASObject = Object.fromEntries(formData);
+
+    try {
+        // we make a fetch POST request to create a todo in the API
+        let response = await fetch("http://localhost:8083/api/todos",
+
+            {
+                method: "POST",
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                // take the data from the form and build the body of the request
+                body: JSON.stringify(formDataASObject)
+            }
+        );
+        // turn the response into somthing we can work with 
+        let newToDo = await response.json();
+
+        console.log(newToDo)
+    } catch (error) {
+
+        console.log("HELP!!!!!!")
 
 
-
+    }
 
 }
